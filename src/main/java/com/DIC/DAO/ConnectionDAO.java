81200cd7e55ce61dc6716f8a0f74eedd2abff51d
visu <viswanatha.reddy2255@gmail.com>
2023-07-22 11:47:49 +0530

    database chaged to prod

--- src/main/java/com/DIC/DAO/ConnectionDAO.java
+++ src/main/java/com/DIC/DAO/ConnectionDAO.java
@@ -9,10 +9,15 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class ConnectionDAO {
 	
-	  //Hani Database
+	  //Hani Prod Database
 		private final static String url="jdbc:postgresql://localhost:5432/postgres";
 		private final static String username="postgres";
 		private final static String password="admin";
+		
+	  //Hani QA Database
+		//private final static String url="jdbc:postgresql://localhost:5432/qa";
+		//private final static String username="postgres";
+		//private final static String password="admin";
 		
 		
 		//New prod
--- visu.bat
+++ visu.bat
@@ -1,7 +1,7 @@
 cd D:\Tomcat9\webapps
 del HansiSoft-10.war
-rmdir /Q /S HansiSoft
-cd D:\HansiSoft\target
+rmdir /Q /S hansisofts
+cd D:\hansisofts\target
 copy HansiSoft-10.war D:\Tomcat9\webapps
-cd D:\HansiSoft
+cd D:\hansisofts
 
