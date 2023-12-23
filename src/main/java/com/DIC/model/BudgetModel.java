899c2c8669a647ded39643b92092761471d502b6
visu <viswanatha.reddy2255@gmail.com>
2023-12-23 12:28:49 +0530

    buget

---
+++ src/main/java/com/DIC/model/BudgetModel.java
@@ -0,0 +1,77 @@
+package com.DIC.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.primefaces.model.StreamedContent;
+
+public class BudgetModel implements Serializable {
+	
+	private String name;
+	private int cost;
+	private String contactNo;
+	private String pro_type;
+	private StreamedContent streamedContent;
+	private Date create_date;
+	private String prim_location;
+	private String seco_location;
+	private String location;
+	
+	public String getName() {
+		return name;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public String getPro_type() {
+		return pro_type;
+	}
+	public StreamedContent getStreamedContent() {
+		return streamedContent;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public void setPro_type(String pro_type) {
+		this.pro_type = pro_type;
+	}
+	public void setStreamedContent(StreamedContent streamedContent) {
+		this.streamedContent = streamedContent;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+	public String getPrim_location() {
+		return prim_location;
+	}
+	public String getSeco_location() {
+		return seco_location;
+	}
+	public void setPrim_location(String prim_location) {
+		this.prim_location = prim_location;
+	}
+	public void setSeco_location(String seco_location) {
+		this.seco_location = seco_location;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+
+}
