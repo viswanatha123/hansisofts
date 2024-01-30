8887405ccc92216f3766429915be6b56b8062970
visu <viswanatha.reddy2255@gmail.com>
2024-01-30 16:36:30 +0530

    new prod

--- src/main/java/com/DIC/DAO/ConnectionDAO.java
+++ src/main/java/com/DIC/DAO/ConnectionDAO.java
@@ -11,12 +11,12 @@ public class ConnectionDAO {
 	
 	
 	  //Hani Prod Database
-		//private final static String url="jdbc:postgresql://localhost:5432/postgres";
+		//private final static String url="jdbc:postgresql://localhost:5432/hansi-prod";
 		//private final static String username="postgres";
 		//private final static String password="admin";
 			
 	  //Hani QA Database
-		private final static String url="jdbc:postgresql://localhost:5432/qa";
+		private final static String url="jdbc:postgresql://localhost:5432/hansi-qa";
 		private final static String username="postgres";
 		private final static String password="admin";
 		
