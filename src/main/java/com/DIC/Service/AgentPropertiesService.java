7326145a77cf7b2c18460b952d15cfcbf0c2cace
visu <viswanatha.reddy2255@gmail.com>
2024-10-30 16:33:17 +0530

    agent

--- src/main/java/com/DIC/Service/AgentPropertiesService.java
+++ src/main/java/com/DIC/Service/AgentPropertiesService.java
@@ -1,16 +1,25 @@
 package com.DIC.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.primefaces.PrimeFaces;
+import org.primefaces.model.StreamedContent;
+
 
 import com.DIC.DAO.Impl.GeneralDAOImpl;
 import com.DIC.DAO.Impl.LocationDAOImpl;
@@ -29,7 +38,9 @@ private static final Logger log = LogManager.getLogger(AgentPropertiesService.cl
 	
 	private String locationMessage;
 	private String errorMessage;
-	private int fetchRecords;
+	private int currentPage = 1;
+	private int pageSize = 10;
+	private int totalRecords;
 	
 	
 
@@ -44,70 +55,94 @@ private static final Logger log = LogManager.getLogger(AgentPropertiesService.cl
 		
 		
 	    GeneralDAOImpl gDao;
-		
-	    //LocationDAOImpl locationDao;
 	    UserDAOImpl udo;
 	    
 	    
 	  
 	
-			@PostConstruct 
-		    public void init()
-		    {
-		    	log.info("Loading Agent Property init()");
-		    	gDao=new GeneralDAOImpl();
-		    	udo=new UserDAOImpl();
-		        
-		 
-		        
-		        villaModel=gDao.getAgentProperties();
-		        fetchRecords=villaModel.size();
-		        
-		        System.out.println("----Record Size :"+villaModel.size());
-		         	
-		    }
+	    public AgentPropertiesService()
+		{
 			
-			@PreDestroy
-			public void destroy()
-			{
-				
-			}
-
-			public void submit() {
-		    	System.out.println("-------------submit ----------------------");
-	        	log.info("Selected property  : "+selectedProperty.getVillaId()+"  "+selectedProperty.getUserId()+"    "+custName+"  "+contactNumber+"    "+email);
-	        	
-	        	
-	        	if(selectedProperty.getVillaId()!=0)
-	        	{
-	        		if(custName!=null && contactNumber!=null && contactNumber!=null)
-	        		{
-	        			if(selectedProperty.getUserId()!=0)
-	        			{
-	        				String saveMessage=udo.saveLeads(custName,contactNumber,email,selectedProperty.getVillaId(),selectedProperty.getUserId(),"villa");
-	        				SMTPService.sendVillaLeadEmail(custName,contactNumber,email,selectedProperty);
-	        				log.info("***** Successful submitted lead ******");
-	        			}
-	        			if(selectedProperty.getUserId()==0)
-	        			{
-	        				int defaultUserId=1;
-	        				String saveMessage=udo.saveLeads(custName,contactNumber,email,selectedProperty.getVillaId(),defaultUserId,"villa");
-	        				log.info("***** Successful submitted lead ******");
-	        			}
-	        			
-	        			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "We received your contact details", "Our representative contact you soon, Thank you..");
-	        	        PrimeFaces.current().dialog().showMessageDynamic(message);
-	        		}
-	        	}
-	        	
-	        	
-	        	villaModel=gDao.getAgentProperties();
-	        	
-	        }
+			log.info("Loading  AgentPropertiesService init()");
+	    	gDao=new GeneralDAOImpl();
+	    	udo=new UserDAOImpl();
 	        
-	   public void reset() {
-		       PrimeFaces.current().resetInputs("form1:panelDialog");
-	  }
+	 
+	        
+	        loadEntities();
+			countTotalRecords();
+			
+		}
+		
+		
+		public void loadEntities() {
+	 		
+			villaModel=gDao.getAgentProperties(pageSize,currentPage);
+	 		
+	        
+	    }
+	 	
+	 	public void countTotalRecords() {
+	 	
+	 		totalRecords=gDao.getAgentPropertiesCountTotalRecords();
+	        
+	    }
+	 	public void nextPage() {
+	        if ((currentPage * pageSize) < totalRecords) {
+	            currentPage++;
+	            loadEntities();
+	        }
+	    }
+
+	    public void previousPage() {
+	        if (currentPage > 1) {
+	            currentPage--;
+	            loadEntities();
+	        }
+	    }
+	 	
+	    public int getTotalPages() {
+	        return (int) Math.ceil((double) totalRecords / pageSize);
+	    }
+		
+		
+		
+		
+
+		public void submit() {
+	    	System.out.println("-------------submit ----------------------");
+        	log.info("Selected property  : "+selectedProperty.getVillaId()+"  "+selectedProperty.getUserId()+"    "+custName+"  "+contactNumber+"    "+email);
+        	
+        	
+        	if(selectedProperty.getVillaId()!=0)
+        	{
+        		if(custName!=null && contactNumber!=null && contactNumber!=null)
+        		{
+        			if(selectedProperty.getUserId()!=0)
+        			{
+        				String saveMessage=udo.saveLeads(custName,contactNumber,email,selectedProperty.getVillaId(),selectedProperty.getUserId(),"villa");
+        				SMTPService.sendVillaLeadEmail(custName,contactNumber,email,selectedProperty);
+        				log.info("***** Successful submitted lead ******");
+        			}
+        			if(selectedProperty.getUserId()==0)
+        			{
+        				int defaultUserId=1;
+        				String saveMessage=udo.saveLeads(custName,contactNumber,email,selectedProperty.getVillaId(),defaultUserId,"villa");
+        				log.info("***** Successful submitted lead ******");
+        			}
+        			
+        			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "We received your contact details", "Our representative contact you soon, Thank you..");
+        	        PrimeFaces.current().dialog().showMessageDynamic(message);
+        		}
+        	}
+        	
+        	
+        	  loadEntities();
+        }
+        
+   public void reset() {
+	       PrimeFaces.current().resetInputs("form1:panelDialog");
+  }
 
 
 			public List<VillaModel> getVillaModel() {
@@ -205,13 +240,36 @@ private static final Logger log = LogManager.getLogger(AgentPropertiesService.cl
 				this.email = email;
 			}
 
-			public int getFetchRecords() {
-				return fetchRecords;
+		
+			public int getCurrentPage() {
+				return currentPage;
 			}
 
-			public void setFetchRecords(int fetchRecords) {
-				this.fetchRecords = fetchRecords;
+
+			public int getPageSize() {
+				return pageSize;
 			}
+
+
+			public int getTotalRecords() {
+				return totalRecords;
+			}
+
+
+			public void setCurrentPage(int currentPage) {
+				this.currentPage = currentPage;
+			}
+
+
+			public void setPageSize(int pageSize) {
+				this.pageSize = pageSize;
+			}
+
+
+			public void setTotalRecords(int totalRecords) {
+				this.totalRecords = totalRecords;
+			}
+		
 		        
         
 
