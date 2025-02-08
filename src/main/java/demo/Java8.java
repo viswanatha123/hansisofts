538685302842e94c26c9abe29a14a4aed13f8daf
visu <viswanatha.reddy2255@gmail.com>
2025-02-08 14:02:29 +0530

    layout

--- src/main/java/demo/Java8.java
+++ src/main/java/demo/Java8.java
@@ -1,20 +1,40 @@
 package demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Java8 {
 
 	public static void main(String[] args) {
 
-	
-				int x[]= {1,2,13,4,5};
-			
-		List<Integer> xx=Arrays.stream(x).boxed().collect(Collectors.toList());
-		List<String> ss=xx.stream().map(n->n+"").filter(n->n.startsWith("1")).collect(Collectors.toList());
-		ss.forEach(n->System.out.println(n));
+		
+		List<Employee> list=new ArrayList<>();
+		list.add(new Employee("aaa",10,1000));
+		list.add(new Employee("bbb",10,1000));
+		list.add(new Employee("bbb",10,1000));
+		list.add(new Employee("ccc",10,1000));
+		list.add(new Employee("ddd",10,1000));
+		
+		Map<String, Long> xx=list.stream().collect(Collectors.groupingBy(Employee::getName,Collectors.counting()));
+		
+		
+		Map<String, Integer> bb=list.stream().collect(Collectors.groupingBy(Employee::getName, Collectors.summingInt(Employee::getAge)));
+		
+		
+		for(Map.Entry<String, Integer> cc: bb.entrySet())
+		{
+			System.out.println(cc.getKey()+"  "+cc.getValue());
+		}
+		
+		
+		
 	}
-
+	
+	
 }
