efca333603fcfb7bc025cada56e6be7e24e908f3
visu <viswanatha.reddy2255@gmail.com>
2025-08-02 10:50:30 +0530

    promoimage

--- src/main/java/com/DIC/DAO/Impl/GeneralDAOImpl.java
+++ src/main/java/com/DIC/DAO/Impl/GeneralDAOImpl.java
@@ -179,7 +179,7 @@ public class GeneralDAOImpl {
 			String SQL_PLOT3BHK_COUNT="select count(*) from villa_plot where bed_rooms='3'";
 			String SQL_PLOT4BHK_PROPERTIES="select *from villa_plot where bed_rooms='4' order by create_date desc LIMIT ? OFFSET ?";
 			String SQL_PLOT4BHK_COUNT="select count(*) from villa_plot where bed_rooms='4'";
-			String SQL_PROMO_IMAGE="select * from promo_img order by display_order";
+			String SQL_PROMO_IMAGE="select * from promo_img order by create_date desc";
 			String SQL_VILLA_COUNT="select count(*) from villa_plot where prim_location = ? and seco_location = ?";
 			String SQL_PROMO_IMAGE_VILLA="select * from promo_img where is_active ='1' order by display_order desc LIMIT ? OFFSET ?";
 			String SQL_DEL_PROMO_IMAGE="delete from promo_img where promo_id=?";
