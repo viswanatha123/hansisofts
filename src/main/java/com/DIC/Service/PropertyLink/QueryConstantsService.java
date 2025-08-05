efab9924792d90176131f08d31a1c5e753f30249
Mitte Likhitha <mittelikhitha2003@example.com>
2025-08-05 20:02:40 +0530

    all

--- src/main/java/com/DIC/Service/PropertyLink/QueryConstantsService.java
+++ src/main/java/com/DIC/Service/PropertyLink/QueryConstantsService.java
@@ -20,14 +20,19 @@ public class QueryConstantsService implements Serializable {
                 public static String param_bangalore_three_bhk="param_bangalore_three_bhk";
                 public static String param_bangalore_three_bhk_count="param_bangalore_three_bhk_count";
 
+                public static String param_bangalore_ready_to_move="param_bangalore_ready_to_move";
+                public static String param_bangalore_ready_to_move_count="param_bangalore_ready_to_move_count";
+
                 public static String param_bangalore_one_bhk_head="1 BHK Flat in Sale";
                 public static String param_bangalore_two_bhk_head="2 BHK Flat in Sale";
                 public static String param_bangalore_three_bhk_head="3 BHK Flat in Sale";
+                public static String param_bangalore_ready_to_move_head="Ready To Move  Bangalore";
 
 
 
 
-            public interface QueryLink{
+
+    public interface QueryLink{
 
                 public static final String  sql_bangalore_one_bhk="select * from villa_plot where bed_rooms='1' order by create_date desc LIMIT ? OFFSET ?";
                 public static final String  sql_bangalore_one_bhk_count="select count(*) from villa_plot where bed_rooms='1'";
@@ -42,9 +47,12 @@ public class QueryConstantsService implements Serializable {
                 public static final String  sql_bangalore_three_bhk_count="select count(*) from villa_plot where bed_rooms='3' and prim_location ='Bangalore'";
 
 
+                public static final String  sql_bangalore_ready_to_move="select * from villa_plot where pro_avail='Ready To Move' and prim_location ='Bangalore' order by create_date desc LIMIT ? OFFSET ?";
+                public static final String  sql_bangalore_ready_to_move_count="select count(*) from villa_plot where pro_avail='Ready To Move' and prim_location ='Bangalore'";
 
 
-            }
+
+    }
 
 
     public String getParam_bangalore_one_bhk() {
@@ -70,7 +78,13 @@ public class QueryConstantsService implements Serializable {
     public  void setParam_bangalore_three_bhk(String param_bangalore_three_bhk) {
         this.param_bangalore_three_bhk = param_bangalore_three_bhk;
     }
+    public  String getParam_bangalore_ready_to_move() {
+        return param_bangalore_ready_to_move;
+    }
 
+    public  void setParam_bangalore_ready_to_move(String param_bangalore_ready_to_move) {
+        this.param_bangalore_ready_to_move = param_bangalore_ready_to_move;
+    }
     public String getParam_bangalore_one_bhk_count() {
         return param_bangalore_one_bhk_count;
     }
@@ -93,6 +107,13 @@ public class QueryConstantsService implements Serializable {
 
     public void setParam_bangalore_three_bhk_count(String param_bangalore_three_bhk_count) {
         this.param_bangalore_three_bhk_count = param_bangalore_three_bhk_count;
+    }
+    public String getParam_bangalore_ready_to_move_count() {
+        return param_bangalore_ready_to_move_count;
+    }
+
+    public void setParam_bangalore_ready_to_move_count(String param_bangalore_ready_to_move_count) {
+        this.param_bangalore_ready_to_move_count = param_bangalore_ready_to_move_count;
     }
 
     public  String getParam_bangalore_one_bhk_head() {
@@ -117,5 +138,12 @@ public class QueryConstantsService implements Serializable {
 
     public void setParam_bangalore_three_bhk_head(String param_bangalore_three_bhk_head) {
         this.param_bangalore_three_bhk_head = param_bangalore_three_bhk_head;
+    }
+    public String getParam_bangalore_ready_to_move_head() {
+        return param_bangalore_ready_to_move_head;
+    }
+
+    public void setParam_bangalore_ready_to_move_head(String param_bangalore_ready_to_move_head) {
+        this.param_bangalore_ready_to_move_head = param_bangalore_ready_to_move_head;
     }
 }
