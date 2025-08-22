f25100decedafbdbc8e133bf3974ce4cd0754887
Girireddy220 <pedaballigirireddy@gmail.com>
2025-08-22 15:38:07 +0530

    modi

--- src/main/java/com/DIC/Service/CornerBitService.java
+++ src/main/java/com/DIC/Service/CornerBitService.java
@@ -4,15 +4,22 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.annotation.PreDestroy;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.primefaces.PrimeFaces;
 
 import com.DIC.DAO.Impl.GeneralDAOImpl;
 import com.DIC.DAO.Impl.UserDAOImpl;
 import com.DIC.model.BudgetModel;
+import com.DIC.model.PromoImageModel;
 
 import SMTPService.SMTPService;
 
@@ -23,12 +30,21 @@ public class CornerBitService implements Serializable {
 	private static final Logger log = Logger.getLogger(CornerBitService.class.getName());
 	
 	private String locationMessage;
-	private List<BudgetModel> budgetModelList;
+
+
+
 	
 	
 	 private int currentPage = 1;
 	 private int pageSize = 10;
 	 private int totalRecords;
+
+	private int promoCurrentPage = 1;
+	private int promoPageSize = 3;
+	private int promoTotalRecords;
+
+	private List<BudgetModel> budgetModelList;
+	private List<PromoImageModel> promoImageModel;
 	 
 	 private BudgetModel selectedProperty;
 	 
@@ -37,8 +53,8 @@ public class CornerBitService implements Serializable {
 	private String contactNumber="";
 	private String email="";
 	
-	GeneralDAOImpl gDao;
-	UserDAOImpl udo;
+	private GeneralDAOImpl gDao;
+	private UserDAOImpl udo;
 	
 	
 		public CornerBitService()
@@ -46,6 +62,30 @@ public class CornerBitService implements Serializable {
 			log.info("Loading CornerBitService Constructor");
 			gDao=new GeneralDAOImpl();
 			udo=new UserDAOImpl();
+
+			FacesContext context = FacesContext.getCurrentInstance();
+			if (context != null) {
+				             HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+				             String pageParam = request.getParameter("cornerbit");
+				           if (pageParam != null && !pageParam.isEmpty()) {
+					                 try {
+						                   currentPage = Integer.parseInt(pageParam);
+						              } catch (NumberFormatException e) {
+						                    currentPage = 1;
+						                  }
+					              }
+
+				            String promoParam = request.getParameter("cornerbitpromo");
+				             if (promoParam != null && !promoParam.isEmpty()) {
+					                  try {
+						                     promoCurrentPage = Integer.parseInt(promoParam);
+						                 } catch (NumberFormatException e) {
+						                      promoCurrentPage = 1;
+						                  }
+					              }
+				          }
+
 			
 			loadEntities();
 			countTotalRecords();
@@ -57,8 +97,12 @@ public class CornerBitService implements Serializable {
 	 	public void loadEntities() {
 	 		
 	 		budgetModelList=gDao.getBudget1Details(5,pageSize,currentPage);
+			promoImageModel = gDao.getPromoImageVilla(promoPageSize, promoCurrentPage);
+
+			log.info("CornerBit properties loaded, count: " +
+					(budgetModelList != null ? budgetModelList.size() : 0));
 	 		
-	 		System.out.println("Cout : "+budgetModelList.size());
+//	 		System.out.println("Cout : "+budgetModelList.size());
 	 		
 	        
 	    }
@@ -66,52 +110,67 @@ public class CornerBitService implements Serializable {
 	 	public void countTotalRecords() {
 	 	
 	 		totalRecords=gDao.getBudget1DetailsCountTotalRecords(5);
+			promoTotalRecords = gDao.getPromoCountTotalRecords();
 	        
 	    }
 	 	
 	 	public void nextPage() {
-	        if ((currentPage * pageSize) < totalRecords) {
-	            currentPage++;
-	            loadEntities();
-	        }
-	    }
+			if ((currentPage * pageSize) < totalRecords) {
+				currentPage++;
+				loadEntities();
+			}
+			if ((promoCurrentPage * promoPageSize) < promoTotalRecords) {
+				promoCurrentPage++;
+				loadEntities();
+			}
+		}
 
 	    public void previousPage() {
 	        if (currentPage > 1) {
 	            currentPage--;
 	            loadEntities();
 	        }
-	    }
+			if (promoCurrentPage > 1) {
+				             promoCurrentPage--;
+				            loadEntities();
+			}
+		}
 	 	
 	    public int getTotalPages() {
 	        return (int) Math.ceil((double) totalRecords / pageSize);
 	    }
+	public void storeSelectedPropertyInSession() {
+		       FacesContext facesContext = FacesContext.getCurrentInstance();
+		         HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+		          session.setAttribute("selectedCornerBitProperty", selectedProperty);
+		    }
 	    
 	    public void submit() {
-	    	System.out.println("-------------submit ----------------------");
-        	log.info("Selected property  : "+selectedProperty.getPro_id()+"  "+selectedProperty.getUserId()+"    "+custName+"  "+contactNumber+"    "+email);
-        	
-        	
-        	if(selectedProperty.getPro_id()!=0)
-        	{
-        		if(custName!=null && contactNumber!=null && contactNumber!=null)
-        		{
-        			if(selectedProperty.getUserId()!=0)
-        			{
-        				String saveMessage=udo.saveLeads(custName,contactNumber,email,selectedProperty.getPro_id(),selectedProperty.getUserId(),selectedProperty.getPro_type());
-        				SMTPService.sendVillaLeadEmail(custName,contactNumber,email,selectedProperty);
-        				log.info("***** Successful submitted lead ******");
-        			}
-        			if(selectedProperty.getUserId()==0)
-        			{
-        				int defaultUserId=1;
-        				String saveMessage=udo.saveLeads(custName,contactNumber,email,selectedProperty.getPro_id(),defaultUserId,selectedProperty.getPro_type());
-        				log.info("***** Successful submitted lead ******");
-        			}
-        			
-        			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "We received your contact details", "Our representative contact you soon, Thank you..");
-        	        PrimeFaces.current().dialog().showMessageDynamic(message);
-        		}
+
+			log.info("Submit called for CornerBit property");
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+			if (session != null) {
+				selectedProperty = (BudgetModel) session.getAttribute("selectedCornerBitProperty");
+			}
+
+//	    	System.out.println("-------------submit ----------------------");
+//        	log.info("Selected property  : "+selectedProperty.getPro_id()+"  "+selectedProperty.getUserId()+"    "+custName+"  "+contactNumber+"    "+email);
+			if (selectedProperty != null && selectedProperty.getPro_id() != 0) {
+				if (custName != null && contactNumber != null) {
+					int userId = (selectedProperty.getUserId() != 0) ? selectedProperty.getUserId() : 1;
+					udo.saveLeads(custName, contactNumber, email,
+							            selectedProperty.getPro_id(),
+							                userId, selectedProperty.getPro_type());
+					       SMTPService.sendVillaLeadEmail(custName, contactNumber, email, selectedProperty);
+					                log.info("***** Successful submitted CornerBit lead ******");
+
+					                  FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+							                          "We received your contact details",
+							                        "Our representative will contact you soon, Thank you..");
+					                 PrimeFaces.current().dialog().showMessageDynamic(message);
+
+				}
         	}
         	
         	
@@ -121,6 +180,13 @@ public class CornerBitService implements Serializable {
 		   public void reset() {
 			       PrimeFaces.current().resetInputs("form1:panelDialog");
 		  }
+
+	@PreDestroy
+ public void cleanup() {
+		       if (budgetModelList != null) {
+			             budgetModelList.clear();
+			        }
+		   }
 	 
 	 	
 	 
@@ -183,6 +249,7 @@ public class CornerBitService implements Serializable {
 
 
 
+
 		public void setSelectedProperty(BudgetModel selectedProperty) {
 			this.selectedProperty = selectedProperty;
 		}
@@ -228,7 +295,13 @@ public class CornerBitService implements Serializable {
 		public void setEmail(String email) {
 			this.email = email;
 		}
-	
-	
+	public int getPromoCurrentPage() { return promoCurrentPage; }
+	public void setPromoCurrentPage(int promoCurrentPage) { this.promoCurrentPage = promoCurrentPage; }
+
+	public int getPromoPageSize() { return promoPageSize; }
+      public void setPromoPageSize(int promoPageSize) { this.promoPageSize = promoPageSize; }
+
+	public List<PromoImageModel> getPromoImageModel() { return promoImageModel; }
+    public void setPromoImageModel(List<PromoImageModel> promoImageModel) { this.promoImageModel = promoImageModel; }
 
 }
