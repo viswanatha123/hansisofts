ffaf71cab7f75f99b920046edcf839fb8a872652
swathi <swathihari63132@gmail.com>
2025-09-01 17:18:40 +0530

    modify

--- src/main/java/com/DIC/DAO/Impl/QueryFactoryManagerImpl.java
+++ src/main/java/com/DIC/DAO/Impl/QueryFactoryManagerImpl.java
@@ -9,35 +9,34 @@ public class QueryFactoryManagerImpl implements QueryFactoryManager {
     @Override
     public String getQueryByParam(String query) {
 
-        System.out.println("******************* xxxxxxxxxxxxx *********************** "+query);
-        String sql_query="";
+        System.out.println("******************* xxxxxxxxxxxxx *********************** " + query);
+        String sql_query = "";
 
 
-        if(query.equals(QueryConstantsService.param_bangalore_one_bhk))
-        {
-            sql_query= QueryConstantsService.QueryLink.sql_bangalore_one_bhk;
+        if (query.equals(QueryConstantsService.param_bangalore_one_bhk)) {
+            sql_query = QueryConstantsService.QueryLink.sql_bangalore_one_bhk;
         }
-        if(query.equals(QueryConstantsService.param_bangalore_two_bhk))
-        {
-            sql_query= QueryConstantsService.QueryLink.sql_bangalore_two_bhk;
+        if (query.equals(QueryConstantsService.param_bangalore_two_bhk)) {
+            sql_query = QueryConstantsService.QueryLink.sql_bangalore_two_bhk;
         }
-        if(query.equals(QueryConstantsService.param_bangalore_three_bhk))
-        {
-            sql_query= QueryConstantsService.QueryLink.sql_bangalore_three_bhk;
+        if (query.equals(QueryConstantsService.param_bangalore_three_bhk)) {
+            sql_query = QueryConstantsService.QueryLink.sql_bangalore_three_bhk;
         }
-        if(query.equals(QueryConstantsService.param_bangalore_ready_to_move))
-        {
-            sql_query= QueryConstantsService.QueryLink.sql_bangalore_ready_to_move;
+        if (query.equals(QueryConstantsService.param_bangalore_ready_to_move)) {
+            sql_query = QueryConstantsService.QueryLink.sql_bangalore_ready_to_move;
         }
+        if (query.equals(QueryConstantsService.param_hyderabad_one_bhk)) {
+            sql_query = QueryConstantsService.QueryLink.sql_hyderabad_one_bhk;
 
-
+        }
+        if (query.equals(QueryConstantsService.param_hyderabad_one_bhk)) {
+            sql_query = QueryConstantsService.QueryLink.sql_hyderabad_one_bhk;
+        }
         return sql_query;
     }
 
 
     public String getQueryCountByParam(String query) {
-
-
 
         String sql_queryCount="";
 
@@ -61,7 +60,15 @@ public class QueryFactoryManagerImpl implements QueryFactoryManager {
             sql_queryCount= QueryConstantsService.QueryLink.sql_bangalore_ready_to_move_count;
 
         }
+        if(query.equals(QueryConstantsService.param_hyderabad_one_bhk_count))
+        {
+            sql_queryCount = QueryConstantsService.QueryLink.sql_hyderabad_one_bhk_count;
+        }
+        if(query.equals(QueryConstantsService.param_hyderabad_one_bhk_count))
+        {
+            sql_queryCount= QueryConstantsService.QueryLink.sql_hyderabad_one_bhk_count;
 
+        }
         return sql_queryCount;
 
     }
