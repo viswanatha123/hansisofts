0670ea68130bfbbf7bc824d73f1c36d9d8ac6299
visu <viswanatha.reddy2255@gmail.com>
2025-09-21 08:36:34 +0530

    refresh

--- src/main/java/com/DIC/Service/UserProfileService.java
+++ src/main/java/com/DIC/Service/UserProfileService.java
@@ -30,70 +30,79 @@ import framework.utilities.SessionUtils;
 @ManagedBean(name="userProfileService")
 @ViewScoped
 public class UserProfileService {
-	
-	
+
+
 	private static final Logger log = Logger.getLogger(UserProfileService.class.getName());
-	
+
 	 private UserDetails userDetails;
 	 private List<UserProfileRoleModel> userProfileRoleModel;
 	 private int listedCount;
 	 private PackageModel packageModel;
-	 
+	private List<AllPropertyList> allPropertyListVal;
+
+	String defaultLastUpdateDate;
+
 	 UserDAOImpl uDao;
-	 
-	 
+
+
 	 private String firstname;
 	 private String lastname;
 	 private String lastWorkDay;
 	 private int packDuration;
-	 
-      
+
+
   	 private int disLeadFlag=0;
-	 
-	 @PostConstruct 
+
+	 @PostConstruct
 	    public void init()
 	    {
+
 	    	log.log(Level.INFO, "Loading UserProfileService init()");
 	    	uDao=new UserDAOImpl();
-	    	
-	    	
-	    	   HttpSession session = SessionUtils.getSession();
-	      	
-		    	if (session != null)
-		    	{
-		    		if(session.getAttribute("userId")!=null)
-		    		{
-		    		    int userId= Integer.parseInt(session.getAttribute("userId").toString());
-		    		    if(userId > 0)
-		    		    {    	
-		          
-		    		    	    packageModel=uDao.getPackageDetails(userId);
-		    		    	    
-		    		    	
-		    		    		userDetails=uDao.getUser(userId);
-		    		    		userDetails.setListLimit(packageModel.getListLimit());
-		    		    		userDetails.setPackName(packageModel.getPackName());
-		    		    		userDetails.setIsEnable(packageModel.getIsEnable());
-		     		    		userProfileRoleModel=uDao.getUserProfileRoles(userId);
-			    		    	listedCount=uDao.getAllPropByUserId(Integer.parseInt(session.getAttribute("userId").toString())).size();
-			    		    	int	remainDays=packageModel.getPackDuration()-Integer.parseInt(session.getAttribute("remainDays").toString());
-			    		    			
-			    		    			LocalDate currentDate = LocalDate.now();
-			    		    			LocalDate futureDate = currentDate.plusDays(remainDays);
-			    		    			System.out.println("Future date : "+futureDate);
-			    		    			lastWorkDay=futureDate.toString();
-			    		    	packDuration=packageModel.getPackDuration();
-			    		    		
-		    		    }
-		    		}
-		    	}
-		    	
-	    	
-		    	
+			findLastUpdatedDate();
+			profileDetails();
 	    }
-	 
-	 	 
-	
+
+	public void findLastUpdatedDate()
+	{
+		defaultLastUpdateDate=uDao.findLastUpdatedDate(SessionUtils.getUserId());
+		System.out.println("Last updated Date : "+defaultLastUpdateDate);
+	}
+
+	 public void profileDetails()
+	 {
+		 HttpSession session = SessionUtils.getSession();
+
+		 if (session != null)
+		 {
+			 if(session.getAttribute("userId")!=null)
+			 {
+				 int userId= Integer.parseInt(session.getAttribute("userId").toString());
+				 if(userId > 0)
+				 {
+
+					 packageModel=uDao.getPackageDetails(userId);
+
+
+					 userDetails=uDao.getUser(userId);
+					 userDetails.setListLimit(packageModel.getListLimit());
+					 userDetails.setPackName(packageModel.getPackName());
+					 userDetails.setIsEnable(packageModel.getIsEnable());
+					 userProfileRoleModel=uDao.getUserProfileRoles(userId);
+					 listedCount=uDao.getAllPropByUserId(Integer.parseInt(session.getAttribute("userId").toString())).size();
+					 int	remainDays=packageModel.getPackDuration()-Integer.parseInt(session.getAttribute("remainDays").toString());
+
+					 LocalDate currentDate = LocalDate.now();
+					 LocalDate futureDate = currentDate.plusDays(remainDays);
+					 System.out.println("Future date : "+futureDate);
+					 lastWorkDay=futureDate.toString();
+					 packDuration=packageModel.getPackDuration();
+
+				 }
+			 }
+		 }
+	 }
+
 
 	public List<UserProfileRoleModel> getUserProfileRoleModel() {
 		return userProfileRoleModel;
@@ -104,18 +113,21 @@ public class UserProfileService {
 		this.userProfileRoleModel = userProfileRoleModel;
 	}
 
-	
-	
-    /*
-    public void save() {
-        FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage("Welcome " + firstname + " " + lastname));
-        
-        log.log(Level.INFO,"***** Save ******"+ firstname + " " + lastname);
-    }
-    */
-    
-    
+	public void refresh() {
+		System.out.println("****************** welcome to refresh **************");
+		profileDetails();
+
+		if(SessionUtils.getUserId() > 0)
+		{
+			allPropertyListVal=uDao.getAllPropByUserId(SessionUtils.getUserId());
+			System.out.println("****************** Size ************** :"+allPropertyListVal.size());
+			int succReco=uDao.updateRefresh(SessionUtils.getUserId());
+			System.out.println("****************** Total records updated ************** :"+succReco);
+			findLastUpdatedDate();
+		}
+	}
+
+
 
     public String getFirstname() {
         return firstname;
@@ -200,10 +212,19 @@ public class UserProfileService {
 	}
 
 
+	public List<AllPropertyList> getAllPropertyListVal() {
+		return allPropertyListVal;
+	}
 
-	
-	
-	 
-	 
+	public void setAllPropertyListVal(List<AllPropertyList> allPropertyListVal) {
+		this.allPropertyListVal = allPropertyListVal;
+	}
 
+	public String getDefaultLastUpdateDate() {
+		return defaultLastUpdateDate;
+	}
+
+	public void setDefaultLastUpdateDate(String defaultLastUpdateDate) {
+		this.defaultLastUpdateDate = defaultLastUpdateDate;
+	}
 }
