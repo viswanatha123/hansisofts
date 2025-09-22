fc585a76e10ef1f75876bb83c4cd8856348da985
galiharshavardhan7 <galiharshavardhan759@gmail.com>
2025-09-22 16:17:33 +0530

    commit

--- src/main/java/com/DIC/Service/PropertyLink/QueryConstantsService.java
+++ src/main/java/com/DIC/Service/PropertyLink/QueryConstantsService.java
@@ -26,6 +26,9 @@ public class QueryConstantsService implements Serializable {
                 public static String param_flats_in_marthahalli="param_flats_in_marthahalli";
                 public static String param_flats_in_marthahalli_count="param_flats_in_marthahalli_count";
 
+                public static String param_flats_in_whitefield = "param_flats_in_whitefield";
+                public static String param_flats_in_whitefield_count = "param_flats_in_whitefield_count";
+
                 public static String param_hyderabad_one_bhk="param_hyderabad_one_bhk";
                 public static String param_hyderabad_one_bhk_count="param_hyderabad_one_bhk_count";
 
@@ -37,6 +40,7 @@ public class QueryConstantsService implements Serializable {
                 public static String param_bangalore_three_bhk_head="3 BHK Flat in Sale";
                 public static String param_bangalore_ready_to_move_head="Ready To Move  Bangalore";
                 public static String param_flats_in_marthahalli_head="Flats in Marthahalli";
+                public static String param_flats_in_whitefield_head = "Flats in Whitefield";
                 public static String param_hyderabad_one_bhk_head="1 BHK Flat in Sale";
 
 
@@ -64,6 +68,9 @@ public class QueryConstantsService implements Serializable {
 
                 public static final String  sql_flats_in_marthahalli="select * from villa_plot where property_type ='Flat' and prim_location ='Bangalore' and seco_location ='Maratha Halli' order by create_date desc LIMIT ? OFFSET ?";
                 public static final String  sql_flats_in_marthahalli_count="select count(*) from villa_plot where property_type ='Flat' and prim_location ='Bangalore' and seco_location ='Maratha Halli'";
+
+                public static final String  sql_flats_in_whitefield = "select * from villa_plot where property_type ='Flat' and prim_location ='Bangalore' and seco_location ='Whitefield' order by create_date desc LIMIT ? OFFSET ?";
+                public static final String  sql_flats_in_whitefield_count = "select count(*) from villa_plot where property_type ='Flat' and prim_location ='Bangalore' and seco_location ='Whitfield'";
 
                 public static final String  sql_hyderabad_one_bhk="select * from villa_plot where bed_rooms='1' and prim_location ='Hyderabad' order by create_date desc LIMIT ? OFFSET ?";
                 public static final String  sql_hyderabad_one_bhk_count="select count(*) from villa_plot where bed_rooms='1' and prim_location ='Hyderabad'";
@@ -110,6 +117,12 @@ public class QueryConstantsService implements Serializable {
         this.param_flats_in_marthahalli = param_flats_in_marthahalli;
     }
 
+    public String getParam_flats_in_whitefield() {return param_flats_in_whitefield; }
+
+    public void setParam_flats_in_whitefield(String param_flats_in_whitefield) {
+        this.param_flats_in_whitefield = param_flats_in_whitefield;
+    }
+
 
 
     public String getParam_bangalore_one_bhk_count() {
@@ -150,6 +163,11 @@ public class QueryConstantsService implements Serializable {
 
     public void setParam_flats_in_marthahalli_count(String param_flats_in_marthahalli_count) {
         this.param_flats_in_marthahalli_count = param_flats_in_marthahalli_count;
+    }
+    public String getParam_flats_in_whitefield_count() {return param_flats_in_whitefield_count;}
+
+    public void setParam_flats_in_whitefield_count(String param_flats_in_whitefield_count) {
+        this.param_flats_in_whitefield_count = param_flats_in_whitefield_count;
     }
 
 
@@ -192,8 +210,16 @@ public class QueryConstantsService implements Serializable {
     public void setParam_flats_in_marthahalli_head(String param_flats_in_marthahalli_head) {
         this.param_flats_in_marthahalli_head = param_flats_in_marthahalli_head;
     }
+    public String getParam_flats_in_whitefield_head() {
+        return param_flats_in_whitefield_head;
+    }
 
-    public String getParam_hyderabad_one_bhk() {
+    public void setParam_flats_in_whitefield_head(String param_flats_in_whitefield_head) {
+        this.param_flats_in_whitefield_head = param_flats_in_whitefield_head;
+    }
+
+
+        public String getParam_hyderabad_one_bhk() {
         return param_hyderabad_one_bhk;
     }
 
