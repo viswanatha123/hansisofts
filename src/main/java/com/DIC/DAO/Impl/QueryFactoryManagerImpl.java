2906db700a873ab03202e15decbd9e199d2b3e9a
galiharshavardhan7 <galiharshavardhan759@gmail.com>
2025-09-22 18:33:41 +0530

    New branch OPS-13

--- src/main/java/com/DIC/DAO/Impl/QueryFactoryManagerImpl.java
+++ src/main/java/com/DIC/DAO/Impl/QueryFactoryManagerImpl.java
@@ -29,6 +29,9 @@ public class QueryFactoryManagerImpl implements QueryFactoryManager {
         {
             sql_query= QueryConstantsService.QueryLink.sql_flats_in_marthahalli;
         }
+        if(query.equals(QueryConstantsService.param_flats_in_whitefield)) {
+            sql_query = QueryConstantsService.QueryLink.sql_flats_in_whitefield;
+        }
         if (query.equals(QueryConstantsService.param_hyderabad_one_bhk)) {
             sql_query = QueryConstantsService.QueryLink.sql_hyderabad_one_bhk;
 
@@ -66,6 +69,9 @@ public class QueryFactoryManagerImpl implements QueryFactoryManager {
         {
             sql_queryCount= QueryConstantsService.QueryLink.sql_flats_in_marthahalli_count;
 
+        }
+        if(query.equals(QueryConstantsService.param_flats_in_whitefield_count)){
+            sql_queryCount = QueryConstantsService.QueryLink.sql_flats_in_whitefield_count;
         }
         if(query.equals(QueryConstantsService.param_hyderabad_one_bhk_count))
         {
