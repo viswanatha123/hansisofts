1a1e3c6367fe1a0ae41e0b5b377f032ab5bcc329
Mitte Likhitha <mittelikhitha2003@example.com>
2025-09-23 14:17:55 +0530

    OPS-52 Properties in Marthahalli

--- src/main/java/com/DIC/DAO/Impl/QueryFactoryManagerImpl.java
+++ src/main/java/com/DIC/DAO/Impl/QueryFactoryManagerImpl.java
@@ -29,6 +29,10 @@ public class QueryFactoryManagerImpl implements QueryFactoryManager {
         {
             sql_query= QueryConstantsService.QueryLink.sql_flats_in_marthahalli;
         }
+        if(query.equals(QueryConstantsService.param_properties_in_marthahalli))
+        {
+            sql_query= QueryConstantsService.QueryLink.sql_properties_in_marthahalli;
+        }
         if (query.equals(QueryConstantsService.param_hyderabad_one_bhk)) {
             sql_query = QueryConstantsService.QueryLink.sql_hyderabad_one_bhk;
 
@@ -65,6 +69,11 @@ public class QueryFactoryManagerImpl implements QueryFactoryManager {
         if(query.equals(QueryConstantsService.param_flats_in_marthahalli_count))
         {
             sql_queryCount= QueryConstantsService.QueryLink.sql_flats_in_marthahalli_count;
+
+        }
+        if(query.equals(QueryConstantsService.param_properties_in_marthahalli_count))
+        {
+            sql_queryCount= QueryConstantsService.QueryLink.sql_properties_in_marthahalli_count;
 
         }
         if(query.equals(QueryConstantsService.param_hyderabad_one_bhk_count))
